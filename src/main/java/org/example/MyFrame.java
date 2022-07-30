81af82ae0d538da8a5d4a22d9f8196a7ba6bad81
Damian Kadziela <damiankadziela02@gmail.com>
2022-07-30 14:20:09 +0200

    Implemented progress slider

--- src/main/java/org/example/MyFrame.java
+++ src/main/java/org/example/MyFrame.java
@@ -3,21 +3,25 @@ package org.example;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
 
-public class MyFrame extends JFrame implements ActionListener {
+public class MyFrame extends JFrame implements ActionListener, MouseListener {
 
     JTextField textField = new JTextField();
     JButton playButton;
-    JButton resetButton;
+    JButton stopButton;
     JButton pauseButton;
     JButton forwardButton;
     JButton backButton;
     JButton loadFileButton;
     Audio audio;
+    JPanel sliderPanel;
+    JSlider slider;
 
 
     Timer timer = new Timer(100, new ActionListener() {
@@ -25,7 +29,10 @@ public class MyFrame extends JFrame implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String actualPosition = String.valueOf(audio.clip.getMicrosecondPosition() / 10000);
             String clipLength = String.valueOf(audio.clip.getMicrosecondLength() / 10000);
-            textField.setText(actualPosition + " / " + clipLength);
+            textField.setText(actualPosition + " / " + clipLength + " / " + slider.getValue());
+            slider.setMaximum(Integer.parseInt(clipLength));
+            slider.setMinimum(0);
+            slider.setValue(Integer.parseInt(actualPosition));
         }
     });
 
@@ -36,11 +43,11 @@ public class MyFrame extends JFrame implements ActionListener {
         playButton.setFocusable(false);
         playButton.setEnabled(false);
 
-        resetButton = new JButton("RESET");
-        resetButton.setBounds(100, 120, 80, 30);
-        resetButton.addActionListener(this);
-        resetButton.setFocusable(false);
-        resetButton.setEnabled(false);
+        stopButton = new JButton("STOP");
+        stopButton.setBounds(100, 120, 80, 30);
+        stopButton.addActionListener(this);
+        stopButton.setFocusable(false);
+        stopButton.setEnabled(false);
 
         pauseButton = new JButton("PAUSE");
         pauseButton.setBounds(180, 120, 80, 30);
@@ -65,6 +72,20 @@ public class MyFrame extends JFrame implements ActionListener {
         loadFileButton.addActionListener(this);
         loadFileButton.setFocusable(false);
 
+        slider = new JSlider(0,6000,0);
+        slider.setOrientation(SwingConstants.HORIZONTAL);
+        slider.setPreferredSize(new Dimension(420,60));
+        slider.setPaintTicks(true);
+        slider.setMajorTickSpacing(6000);
+        slider.setMinorTickSpacing(1000);
+        slider.setPaintLabels(true);
+        slider.addMouseListener(this);
+
+        sliderPanel = new JPanel();
+        sliderPanel.setBounds(10,50,420,80);
+        //sliderPanel.setBackground(Color.red);
+        sliderPanel.add(slider);
+
         //  textField.setText();
         textField.setBounds(20, 20, 160, 30);
         textField.setOpaque(true);
@@ -72,16 +93,18 @@ public class MyFrame extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Lame Wave Player");
         this.setLayout(null);
-        this.setResizable(false);
-        this.setSize(450, 200);
-        this.setVisible(true);
+        this.setResizable(true);
+
         this.add(textField);
         this.add(playButton);
-        this.add(resetButton);
+        this.add(stopButton);
         this.add(pauseButton);
         this.add(forwardButton);
         this.add(backButton);
         this.add(loadFileButton);
+        this.add(sliderPanel);
+        this.setSize(455, 200);
+        this.setVisible(true);
     }
 
     @Override
@@ -90,7 +113,7 @@ public class MyFrame extends JFrame implements ActionListener {
             audio.start();
             timer.start();
         }
-        if (e.getSource() == resetButton) {
+        if (e.getSource() == stopButton) {
             audio.reset();
         }
         if (e.getSource() == pauseButton) {
@@ -114,11 +137,38 @@ public class MyFrame extends JFrame implements ActionListener {
                     throw new RuntimeException(ex);
                 }
                 playButton.setEnabled(true);
-                resetButton.setEnabled(true);
+                stopButton.setEnabled(true);
                 pauseButton.setEnabled(true);
                 forwardButton.setEnabled(true);
                 backButton.setEnabled(true);
             }
         }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        timer.stop();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        audio.set(slider.getValue());
+        System.out.println(slider.getValue());
+        timer.start();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
